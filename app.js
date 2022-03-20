c4c759499552d3d1f121bc851bfd3d1b589a6ea2
Alvaro Fernandez <afernandez@itba.edu.ar>
2022-03-20 15:09:37 -0400

    Agregué la lógica del juego al codigo de JavaScript.

--- app.js
+++ app.js
@@ -14,4 +14,66 @@ let opciones = ["Piedra","Papel","Tijera","Lagartija","Spock"];
 
 let computerChoice = opciones[Math.floor(Math.random()*5)]
 
-console.log(computerChoice)
+let userChoice = prompt("Ingrese su elección (Piedra/Papel/Tijera/Lagartija/Spock):");
+let matchResult;
+
+if (userChoice == computerChoice){
+    matchResult = "Empate";
+}
+else {
+    if (computerChoice=="Piedra") {
+        if (userChoice=="Papel") {
+            matchResult = "¡Ganaste!";
+        } else if (userChoice=="Tijera") {
+            matchResult = "Perdiste :(";
+        } else if (userChoice=="Lagartija") {
+            matchResult = "Perdiste :(";
+        } else if (userChoice=="Spock") {
+            matchResult = "¡Ganaste!";
+        }
+    } else if(computerChoice=="Papel") {
+        if (userChoice=="Piedra") {
+            matchResult = "Perdiste :(";
+        } else if (userChoice=="Tijera") {
+            matchResult = "¡Ganaste!";
+        } else if (userChoice=="Lagartija") {
+            matchResult = "¡Ganaste!";
+        } else if (userChoice=="Spock") {
+            matchResult = "Perdiste :(";
+        }
+    } else if(computerChoice=="Tijera") {
+        if (userChoice=="Piedra") {
+            matchResult = "¡Ganaste!";
+        } else if (userChoice=="Papel") {
+            matchResult = "Perdiste :(";
+        } else if (userChoice=="Lagartija") {
+            matchResult = "Perdiste :(";
+        } else if (userChoice=="Spock") {
+            matchResult = "¡Ganaste!";
+        }
+    } else if(computerChoice=="Lagartija") {
+        if (userChoice=="Piedra") {
+            matchResult = "¡Ganaste!";
+        } else if (userChoice=="Papel") {
+            matchResult = "Perdiste :(";
+        } else if (userChoice=="Tijera") {
+            matchResult = "¡Ganaste!";
+        } else if (userChoice=="Spock") {
+            matchResult = "Perdiste :(";
+        }
+    } else if(computerChoice=="Spock"); {
+        if (userChoice=="Piedra") {
+            matchResult = "Perdiste :(";
+        } else if (userChoice=="Papel") {
+            matchResult = "¡Ganaste!";
+        } else if (userChoice=="Tijera") {
+            matchResult = "Perdiste :(";
+        } else if (userChoice=="Lagartija") {
+            matchResult = "¡Ganaste!";
+        }
+    }
+}
+
+console.log("Elegiste " + userChoice)
+console.log("La computadora eligio " + computerChoice)
+console.log(matchResult)
