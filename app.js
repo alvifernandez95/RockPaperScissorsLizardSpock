311f095fbdcf57e1fe35675da4521f4cf2683999
Alvaro Fernandez <afernandez@itba.edu.ar>
2022-03-22 08:49:44 -0400

    Hice que el juego funcione con los iconos en lugar de con el prompt.

--- app.js
+++ app.js
@@ -10,70 +10,69 @@
 //la lagartija se come el papel, el papel refuta a Spock, Spock vaporiza la piedra y como
 //siempre, la piedra aplasta a las tijeras. 
 
-let opciones = ["Piedra","Papel","Tijera","Lagartija","Spock"];
+function playRPSLS (userChoice) {
+    let opciones = ["Piedra","Papel","Tijera","Lagartija","Spock"];
+    let computerChoice = opciones[Math.floor(Math.random()*5)]
+    //let userChoice = prompt("Ingrese su elección (Piedra/Papel/Tijera/Lagartija/Spock):");
+    let matchResult;
 
-let computerChoice = opciones[Math.floor(Math.random()*5)]
-
-let userChoice = prompt("Ingrese su elección (Piedra/Papel/Tijera/Lagartija/Spock):");
-let matchResult;
-
-if (userChoice == computerChoice){
-    matchResult = "Empate";
-}
-else {
-    if (computerChoice=="Piedra") {
-        if (userChoice=="Papel") {
-            matchResult = "¡Ganaste!";
-        } else if (userChoice=="Tijera") {
-            matchResult = "Perdiste :(";
-        } else if (userChoice=="Lagartija") {
-            matchResult = "Perdiste :(";
-        } else if (userChoice=="Spock") {
-            matchResult = "¡Ganaste!";
-        }
-    } else if(computerChoice=="Papel") {
-        if (userChoice=="Piedra") {
-            matchResult = "Perdiste :(";
-        } else if (userChoice=="Tijera") {
-            matchResult = "¡Ganaste!";
-        } else if (userChoice=="Lagartija") {
-            matchResult = "¡Ganaste!";
-        } else if (userChoice=="Spock") {
-            matchResult = "Perdiste :(";
-        }
-    } else if(computerChoice=="Tijera") {
-        if (userChoice=="Piedra") {
-            matchResult = "¡Ganaste!";
-        } else if (userChoice=="Papel") {
-            matchResult = "Perdiste :(";
-        } else if (userChoice=="Lagartija") {
-            matchResult = "Perdiste :(";
-        } else if (userChoice=="Spock") {
-            matchResult = "¡Ganaste!";
-        }
-    } else if(computerChoice=="Lagartija") {
-        if (userChoice=="Piedra") {
-            matchResult = "¡Ganaste!";
-        } else if (userChoice=="Papel") {
-            matchResult = "Perdiste :(";
-        } else if (userChoice=="Tijera") {
-            matchResult = "¡Ganaste!";
-        } else if (userChoice=="Spock") {
-            matchResult = "Perdiste :(";
-        }
-    } else if(computerChoice=="Spock") {
-        if (userChoice=="Piedra") {
-            matchResult = "Perdiste :(";
-        } else if (userChoice=="Papel") {
-            matchResult = "¡Ganaste!";
-        } else if (userChoice=="Tijera") {
-            matchResult = "Perdiste :(";
-        } else if (userChoice=="Lagartija") {
-            matchResult = "¡Ganaste!";
+    if (userChoice == computerChoice){
+        matchResult = "Empate";
+    }
+    else {
+        if (computerChoice=="Piedra") {
+            if (userChoice=="Papel") {
+                matchResult = "¡Ganaste!";
+            } else if (userChoice=="Tijera") {
+                matchResult = "Perdiste :(";
+            } else if (userChoice=="Lagartija") {
+                matchResult = "Perdiste :(";
+            } else if (userChoice=="Spock") {
+                matchResult = "¡Ganaste!";
+            }
+        } else if(computerChoice=="Papel") {
+            if (userChoice=="Piedra") {
+                matchResult = "Perdiste :(";
+            } else if (userChoice=="Tijera") {
+                matchResult = "¡Ganaste!";
+            } else if (userChoice=="Lagartija") {
+                matchResult = "¡Ganaste!";
+            } else if (userChoice=="Spock") {
+                matchResult = "Perdiste :(";
+            }
+        } else if(computerChoice=="Tijera") {
+            if (userChoice=="Piedra") {
+                matchResult = "¡Ganaste!";
+            } else if (userChoice=="Papel") {
+                matchResult = "Perdiste :(";
+            } else if (userChoice=="Lagartija") {
+                matchResult = "Perdiste :(";
+            } else if (userChoice=="Spock") {
+                matchResult = "¡Ganaste!";
+            }
+        } else if(computerChoice=="Lagartija") {
+            if (userChoice=="Piedra") {
+                matchResult = "¡Ganaste!";
+            } else if (userChoice=="Papel") {
+                matchResult = "Perdiste :(";
+            } else if (userChoice=="Tijera") {
+                matchResult = "¡Ganaste!";
+            } else if (userChoice=="Spock") {
+                matchResult = "Perdiste :(";
+            }
+        } else if(computerChoice=="Spock") {
+            if (userChoice=="Piedra") {
+                matchResult = "Perdiste :(";
+            } else if (userChoice=="Papel") {
+                matchResult = "¡Ganaste!";
+            } else if (userChoice=="Tijera") {
+                matchResult = "Perdiste :(";
+            } else if (userChoice=="Lagartija") {
+                matchResult = "¡Ganaste!";
+            }
         }
     }
+    document.getElementById("userChoice").innerHTML = userChoice;
+    document.getElementById("computerChoice").innerHTML = computerChoice;
+    document.getElementById("matchResult").innerHTML = matchResult;
 }
-
-document.getElementById("userChoice").innerHTML = userChoice;
-document.getElementById("computerChoice").innerHTML = computerChoice;
-document.getElementById("matchResult").innerHTML = matchResult;
